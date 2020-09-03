dbf281df962017a2f4130b5debb122f4cad7b48f
Ivan Salmin <salmin.iv@gmail.com>
2020-09-03 13:37:56 +0400

    Добавляет блок Абонементы

--- source/js/paragraph-truncate.js
+++ source/js/paragraph-truncate.js
@@ -1,10 +1,11 @@
 'use strict';
+
 (function () {
   var gymInfoParagraphs = document.querySelectorAll('.gym__info > p');
 
   var ClassName = {
     VISUALLY_HIDDEN: 'visually-hidden',
-  }
+  };
 
   var ViewportWidth = {
     MAX_TABLET: '(max-width: 1199px)',
@@ -20,12 +21,12 @@
     var truncParagraph = function () {
       for (var i = PARAGRAPHS_TO_KEEP; i < gymInfoParagraphs.length; i++) {
         if (MediaQuery.MAX_TABLET_WIDTH.matches && gymInfoParagraphs.length > PARAGRAPHS_TO_KEEP) {
-          gymInfoParagraphs[i].classList.add(ClassName.VISUALLY_HIDDEN)
+          gymInfoParagraphs[i].classList.add(ClassName.VISUALLY_HIDDEN);
         } else {
-          gymInfoParagraphs[i].classList.remove(ClassName.VISUALLY_HIDDEN)
+          gymInfoParagraphs[i].classList.remove(ClassName.VISUALLY_HIDDEN);
         }
       }
-    }
+    };
 
     truncParagraph();
     window.addEventListener('resize', truncParagraph);
